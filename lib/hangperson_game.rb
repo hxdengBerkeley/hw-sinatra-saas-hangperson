89e9af8ac3aec35abc286d4e04b8d0cabfaa2d9e
Hanxiao Deng <hxdeng@berkeley.edu>
2017-09-08 18:49:58 +0000

    finish git new.erb

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -3,13 +3,81 @@ class HangpersonGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
+#@word #store the answer
+#@guesses #store the chars user had guessed
+#@wrong_guesses #store the chars user wrongly guessed
+#@word_with_guesses
+#@check_win_or_lose
+  # Return the word of HangpersonGame
+  def word
+    return @word
+  end
+  # Return the guesses of HangpersonGame
+  def guesses
+    return @guesses
+  end
+  # Return the guesses of HangpersonGame
+  def wrong_guesses
+    return @wrong_guesses
+  end
+  # guess word once a letter
+  def guess(char)
+    if char==''||!(char=~/[A-Za-z]/)
+      raise ArgumentError
+    else
+      char = char.downcase
+      if @guesses.include?(char)||@wrong_guesses.include?(char)
+        return false
+      else
+        if @word.include?(char)
+          @guesses+=char
+          for i in 0..@word.length-1
+            if @word[i]==char
+              @word_with_guesses[i]=char
+            end
+          end
+          if !@word_with_guesses.include?('-')
+            @check_win_or_lose=:win
+          end
+        else
+          @wrong_guesses+=char
+          if @wrong_guesses.length>=7
+            @check_win_or_lose=:lose
+          end
+        end
+        return true
+      end
+    end
+  end
+  
+  # return word_with_guesses
+  def word_with_guesses
+    return @word_with_guesses
+  end
+
+  # check win or lose
+  def check_win_or_lose
+    return @check_win_or_lose
+  end
+
   # Get a word from remote "random word" service
 
   # def initialize()
+  def initialize()
+    @word = ''
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '-'*word.length
+    @check_win_or_lose = :play
+  end
   # end
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '-'*word.length
+    @check_win_or_lose = :play
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -22,19 +90,5 @@ class HangpersonGame
     Net::HTTP.new('watchout4snakes.com').start { |http|
       return http.post(uri, "").body
     }
-  end
-  
-  # Return the word of HangpersonGame
-  def word
-    return @word
-  end
-  
-  # Return the guesses of HangpersonGame
-  def guesses
-    return ''
-  end
-  # Return the guesses of HangpersonGame
-  def wrong_guesses
-    return ''
   end
 end
